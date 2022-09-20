5a81ae31cd9f9083af0fb77ebd5e1bbc7323f1dd
Lance Noble <ltnlikespie@gmail.com>
2022-09-19 23:30:26 -0400

    Started implementing our concepts for puzzle1 I made a new scene in our unity project called puzzle 1 It's a 2D scene containing a circle (the knot) with a line (the rope) attached to it The goal for this commit was to get mouse detection working so now whenever you hover the mouse over the rope, the game now knows that the mouse is over the rope (check the console). Next steps are to detect if the mouse is over the rope and is being clicked while over it. If so, then start tracking the mouse position to see if it's going in a certain direction. One direction will be for loosening the knot and the other direction will be for tightening the knot

---
+++ igme320FinalProject/Assets/Scripts/Untie.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class Untie : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // determine which direction is loosen and which direction is pull
+        int looseDirection = Random.Range(0, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if left mouse button is down
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("clicked");
+            // check if mouse is over object
+
+            
+        }
+
+        //struct used to get information back from a raycast
+        RaycastHit hit;
+
+        //returns a ray going from camera to mouse position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        //Physics.Raycast returns bool saying whether or not ray intersects with collider
+        //  casts a ray from point origin, in direction of direction parameter, of length maxdistance parameter, against all colliders in scene
+        //  out keyword: used to pass arguments to methods as a reference type, generally used when a method needs to return multiple values
+        //  in this case, we want the function to return not just the bool but also the hit since we haven't initialized it yet
+        //  thanks to "out" the hit variable gets initialized for us by function without us having to do anything
+        //  There is a way to implement this in 2D with Physics2D.Raycast, but idk how to so i'm sticking with raycasting in 3d
+        if (Physics.Raycast(ray, out hit))
+        {
+            // hit variable contains information about the object that our ray (Camera.main.ScreenPointToRay(Input.mousePosition);) just hit
+            //if (hit.transform.name == "rightRope")
+            //{
+            //    Debug.Log("You hit: " + hit.collider.gameObject);
+            //}
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}
