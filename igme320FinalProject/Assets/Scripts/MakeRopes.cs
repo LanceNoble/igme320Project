84cfb9cdfba91f6738c8b46a61e2c6745cc848bd
Lance Noble <ltnlikespie@gmail.com>
2022-09-26 01:45:17 -0400

    made it so that ropes spawn at unique angles instead of the same angle at random times.

--- igme320FinalProject/Assets/Scripts/MakeRopes.cs
+++ igme320FinalProject/Assets/Scripts/MakeRopes.cs
@@ -1,29 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MakeRopes : MonoBehaviour
 {
     public GameObject rope;
+    public int ropeAm;
+    List<GameObject> ropes;
+    bool loosen;
     // Start is called before the first frame update
     void Start()
     {
-        List<GameObject> ropes = new List<GameObject>();
-        for (int i = 0; i < 6; i++)
+        ropes = new List<GameObject>();
+        for (int i = 0; i < ropeAm; i++)
         {
-            //must account for ropes being spawned at the same angle, run the rng again so they get spawned at a more unique angle
-            //create some sort of visual for ordering system, maybe start with colors?
-            //make the ropes move when clicked on in the right order
-            //GameObject rope = Instantiate(this.rope, gameObject.transform.position, Quaternion.identity);
-            //float rng = Random.Range(0.0f, 2 * Mathf.PI);
-            float rng = UnityEngine.Random.Range(0.0f, 360.0f);
-            GameObject rope = Instantiate(this.rope, new Vector3(Mathf.Cos(Mathf.Deg2Rad * rng), Mathf.Sin(Mathf.Deg2Rad * rng), 0.0f), Quaternion.identity);
-            //rope.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f), Random.Range(0.0f, 360.0f));
-            rope.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f), rng);
+            GameObject rope = Instantiate(this.rope, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+            float rngAng = UnityEngine.Random.Range(0.0f, 360.0f);
+           
+            for (int j = 0; j < ropes.Count; j++)
+            {
+               
+                if (rngAng <= ropes[j].transform.rotation.eulerAngles.z + 20.0f && rngAng >= ropes[j].transform.eulerAngles.z - 20.0f)
+                {
+                    rngAng = UnityEngine.Random.Range(0.0f, 360.0f);
+                    j = 0;
+                }
+            }
+            
+            rope.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f), rngAng);
+            Vector3 ropePos = new Vector3(Mathf.Cos(Mathf.Deg2Rad * rngAng), Mathf.Sin(Mathf.Deg2Rad * rngAng), 0.0f);
+           
+            rope.transform.position = ropePos;
+            
             rope.transform.position *= (rope.transform.localScale.x / 2);
             rope.transform.SetParent(gameObject.transform);
-            //rope.transform.position = new Vector3(rope.transform.position.x, rope.transform.position.y, 5.0f);
+            
             rope.GetComponent<SpriteRenderer>().sortingOrder = -1;
             ropes.Add(rope);
         }
@@ -31,16 +44,28 @@ public class MakeRopes : MonoBehaviour
         {
             ropes[i].name = $"rope{i}";
         }
-        foreach(GameObject rope in ropes)
-        {
-            //rope.GetComponent<MeshRenderer>().material.SetColor("bruh", Int32.Parse(rope.name.Substring(4, 1)));
-            rope.GetComponent<SpriteRenderer>().material.color = new Color(1, 0, 1, 1);
-        }
+        //foreach(GameObject rope in ropes)
+        //{
+        //    
+        //    rope.GetComponent<SpriteRenderer>().material.color = new Color(1, 0, 1, 1);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (loosen)
+        {
+            foreach (GameObject rope in ropes)
+            {
+                rope.transform.Translate(new Vector3(5.0f, 0, 0) * Time.deltaTime);
+            }
+        }
     }
+
+    //private void OnMouseDown()
+    //{
+    //    loosen = true;
+    //    
+    //}
 }
